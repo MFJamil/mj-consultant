4c067772245a0d2115a80efc3459dfbdd1b86113
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-11 17:21:07 +0200

    Another test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto">
+    <SiteNavigation /> 
     
-    <Header />
 
     <RouterView />
     <Footer />
@@ -10,7 +10,13 @@
   
 </template>
 <script lang="ts" setup>
-/* <SiteNavigation />*/
+/* 
+
+<SiteNavigation />
+<Header />
+
+
+*/
 import { RouterView } from 'vue-router';
 import SiteNavigation from './components/SiteNavigation.vue';
 import Footer from './components/Footer.vue'
