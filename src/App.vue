864a13cdedb3bdad91f0c5010f5022ed10452f92
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-11 20:52:12 +0200

    BugFix

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ import SiteNavigation from './components/SiteNavigation.vue';
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
 import TestApp from './components/TestApp.vue'
-import { title } from 'process';
+
 import { ref } from 'vue';
 const menu = ref([
   {id: 'mnu_Info', title: 'Info'},
