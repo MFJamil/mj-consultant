a913bfecd5c16fa1f1dec4cfdbe5c069447826cf
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-12 13:29:10 +0200

    IOS BufFix 14

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@
 <TestApp />
   -->
   <div class="flex flex-col min-h-screen font-Roboto">
-    <span class="text-white">Before--2</span> 
-    <Header_IOS :menu="menu" title="MJ Consultant" icon-file="assets/Logo_MJ_shadow_white.png" /> 
-    <span class="text-white">After--2</span> 
+    <span class="text-white">Before--3</span> 
+    <Header_IOS /> 
+    <span class="text-white">After--3</span> 
     <div class="flex flex-col min-h-screen font-Roboto my-60">
       <RouterView />
 
