431fcbf3561e8143498db147b79161f4b9082315
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-12 13:54:10 +0200

    Fix for IOS

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
 <TestApp />
   -->
   <div class="flex flex-col min-h-screen font-Roboto">
-    <span class="text-white">Before--7</span> 
     <Header_IOS :menu="menu" title="MJ Consultant" icon-file="assets/Logo_MJ_shadow_white.png" /> 
-    <span class="text-white">After--7</span> 
     <div class="flex flex-col min-h-screen font-Roboto my-60">
       <RouterView />
 
