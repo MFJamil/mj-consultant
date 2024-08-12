7efca34c1cfdb994445eee9ac370dbb945c5dc63
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-12 20:21:54 +0200

    Background update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,7 @@
 
   <section class="bg-white rounded-2xl shadow-5xl">
       <div class="gemini container flex w-full flex-col sm:flex-row items-center gap-12 text-black py-20" >
-        <div class="flex flex-1" ><span class="mb-4 lg:text-5xl !leading-tight text-3xl font-bold py-0">IT Consultant</span></div>
+        <div class="flex flex-1" ><span class="mb-4 lg:text-5xl !leading-tight text-3xl font-sans font-extrabold py-0">IT Consultant</span></div>
         <div class="flex justify-end" >
           <img src="../assets/MJ_2.png" class="rounded-3xl size-1/2 opacity-50" />
         </div>
