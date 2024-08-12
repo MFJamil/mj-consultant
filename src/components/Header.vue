5c7db8ee8cbb0838f23dbdadf31ee1a8c683efab
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-12 12:22:52 +0200

    IOS BugFix 6

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="top-0 bg-weather-primary z-50 block sticky backdrop-saturate-200 backdrop-blur-2xl w-full max-w-full"
     :style="`position: -webkit-sticky;height: ${navHeight}px !important;background-color:rgba(255,255,255,${opacity}) !important;box-shadow: 0 10px 15px -3px rgb(0 0 0 / ${shadowOp}), 0 4px 6px -4px rgb(0 0 0 / ${shadowOp});`"
-    >
+    >ttt
     <nav class="text-black block">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
