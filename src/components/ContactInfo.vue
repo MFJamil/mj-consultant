54fcd0c521c541e41e62c1b51523dcb2e65cc615
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-16 12:07:20 +0200

    Contact Info update

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,11 +1,12 @@
  <template>
-   
-    <div class="container flex items-center w-full h-auto  justify-center bg-white text-black rounded-2xl py-10" >
-        
-        <img src="../assets/icon-mobile.png" />
-        
-        <img src="../assets/icon-email.png" />
-    </div>
+   <div class="container flex items-center w-full h-auto  justify-center text-black rounded-2xl py-10 border-2 backdrop-saturate-200 backdrop-blur-2xl" >
+      <div class="w-1/2 justify-center flex">
+         <a href="tel:015209511152"><img src="../assets/icon-mobile.png" class="cursor-pointer"/></a> 
+      </div>
+      <div class="w-1/2 justify-center flex">
+         <a href="mailto:info@mj-consultant.de"> <img src="../assets/icon-email.png" class="cursor-pointer" /></a>
+      </div>
+   </div>
  </template>
  
  <script lang="ts">
