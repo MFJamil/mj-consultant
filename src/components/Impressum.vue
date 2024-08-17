36aff0b759514fad1ff20103b2dec7d89690774f
MFJamil <36378263+MFJamil@users.noreply.github.com>
2024-08-17 21:30:34 +0200

    Overflow Update 1

--- src/components/Impressum.vue
+++ src/components/Impressum.vue
@@ -1,9 +1,9 @@
  <template>
-    <div class="bg-white opacity-100 z-50 absolute overflow-y-auto rounded-3xl ring-white ring-2">
+    <div class="bg-white opacity-100 z-50 rounded-3xl absolute overflow-hidden ring-white ring-2">
         <div class="w-full bg-blue-400 h-10">
             <li class="right-5 top-3 absolute pi pi-times font-bold cursor-pointer hover:text-white" @click="$emit('doClose')"></li> 
         </div>
-        
+        <div class="overflow-auto">
         <h1>Impressum</h1>
         <p>Mohammed Jamil<br />
             Walpodenstr, 10<br />
@@ -24,6 +24,7 @@
         <p>Wir nehmen an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teil. Zust&auml;ndig ist die Universalschlichtungsstelle des Zentrums f&uuml;r Schlichtung e.V., Stra&szlig;burger Stra&szlig;e 8, 77694 Kehl am Rhein (<a href="https://www.verbraucher-schlichter.de" rel="noopener noreferrer" target="_blank">https://www.verbraucher-schlichter.de</a>).</p>
 
         <p>Quelle: <a href="https://www.e-recht24.de">eRecht24</a></p>
+        </div>
 
     </div>
 
